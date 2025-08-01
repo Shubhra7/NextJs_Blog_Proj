d26f2bd69cec0b7373a164ef296c2a3469ff7ecd
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-08-01 15:24:02 +0530

    done

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,32 +1,18 @@
-'use client';
 import styles from "../page.module.css";
 import Link from "next/link";
-import { useState } from "react";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <>
-      <nav className={styles.mainnav}>
-        <div className={styles.navContainer}>
-          <button className={styles.hamburger} onClick={toggleMenu}>
-            ☰
-          </button>
-          <ul className={`${styles.navLinks} ${isOpen ? styles.showMenu : ''}`}>
-            <li><Link href='/'>Home</Link></li>
-            <li><Link href='/blog'>Blog</Link></li>
-            <li><Link href='/about'>About</Link></li>
-            <li><Link href='/contact'>Contact</Link></li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-};
-
-export default Navbar;
+  <>
+    <nav className={styles.mainnav}>
+        <ul>
+          <Link href='/'><li>Home</li></Link>
+          <Link href='/blog'><li>Blog</li></Link>
+          <Link href='/about'><li>About</li></Link>
+          <Link href='/contact'><li>Contact</li></Link>
+        </ul>
+    </nav>
+  </>
+  )
+}
+export default Navbar
